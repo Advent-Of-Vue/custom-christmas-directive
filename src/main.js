70a3fc0c3c2398e75512ac6179c946e5d69194bd
mattmaribojoc <matthewmaribojoc@gmail.com>
2022-12-16 09:37:55 -0500

    add solution

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import './base.css'
 
 const app = createApp(App)
 
-app.directive('christmas', (/* todo*/) => {
-  /* todo*/
+app.directive('christmas', (el, binding) => {
+  const duration = binding.value ?? 2 // the length of the animation in seconds
+  const color = binding.arg ?? 'red-green' // the class to add for the different colors
+
+  // this will be called for both `mounted` and `updated`
+  el.classList.add('christmas-text', color)
+
+  el.style.animationDuration = duration + 's' // change the animation depending on the length
 })
 
 app.mount('#app')
